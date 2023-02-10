da65e27d9ca89689930b9a67af8de47bf6bbaeeb
Ragrats- <91531302+ragrats1403@users.noreply.github.com>
2023-02-10 16:27:56 +0800

    added Function

--- app/Http/Controllers/AjaxDataController.php
+++ app/Http/Controllers/AjaxDataController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
 class AjaxDataController extends Controller
 {
     //
+
+    protected function fetchdata()
+    {
+        
+    }
 }
