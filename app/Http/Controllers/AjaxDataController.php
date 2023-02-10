d63bc77189df68f2d42da3ba4b06728eabb519ac
Ragrats- <91531302+ragrats1403@users.noreply.github.com>
2023-02-10 17:00:46 +0800

    Readded Controller

---
+++ app/Http/Controllers/AjaxDataController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AjaxDataController extends Controller
+{
+    //
+    protected function fetchdata()
+    {
+
+    }
+    
+    protected function adddata()
+    {
+        
+    }
+}
