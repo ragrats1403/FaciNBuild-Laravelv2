1bb81966fa946332a7292e645c9b27795f649dda
Ragrats- <91531302+ragrats1403@users.noreply.github.com>
2023-02-14 20:12:12 +0800

    saved resource[1/5]

--- app/Http/Controllers/AjaxDataController.php
+++ app/Http/Controllers/AjaxDataController.php
@@ -3,16 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class AjaxDataController extends Controller
 {
     //
-    protected function fetchdata()
+    protected function insertdata(Request $request)
     {
+        $name = $request->name;
+        $username = $request->username;;
+        $password = $request->password;
+        $rolelevel = $request->rolelevel;
+        $roleid = $request->roleid;
 
+
+        //$sql = "INSERT INTO `users` (`username`,`password`,`rolelevel`,`roleid`,`name`) VALUES ('$username','$password','$rolelevel','$roleid','$name')";   
+        //$query = DB::table('users')
+
+        $query = DB::insert('insert into users (username, password, rolelevel, roleid, name) values (?, ?, ?, ?, ?)', [$username, $password, $rolelevel, $roleid, $name]);
+        if ($query == true) {
+            $data = array(
+                'status' => 'success',
+            );
+            echo json_encode($data);
+        } else {
+        
+            $data = array(
+                'status' => 'failed',
+            );
+            echo json_encode($data);
+        }
     }
     
-    protected function adddata()
+    protected function fetchdata()
     {
         
     }
